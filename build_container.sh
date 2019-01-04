a6f798223e83045d8171db85f4194188e2f193ee
kapsner <44973495+kapsner@users.noreply.github.com>
2019-01-04 22:59:14 +0100

    fixed permissions

--- build_container.sh
+++ build_container.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 git clone https://github.com/kapsner/docker-base_image.git
 cd docker-base_image
+chmod +x build_base_image.sh
 /bin/bash build_base_image.sh
 cd ../r_base_image
+chmod +x build_r_base_image.sh
 /bin/bash build_r_base_image.sh
 cd ..
 docker-compose up --build -d
