71484d988cf192eb7ee7cc824a3f69b9ff6f7771
kapsner <lorenz.kapsner@web.de>
2020-01-12 18:55:30 +0100

    fixed wrong path

--- Rdatascience/build_rdatascience.sh
+++ Rdatascience/build_rdatascience.sh
@@ -2,5 +2,5 @@
 
 printf "\nBuild r_datascience\n"
 docker build -f Dockerfile -t rdsc_base .
-#docker build -f rdc_gpu/Dockerfile -t rdsc_gpu .
-#docker build -f rdc_bio/Dockerfile -t rdsc_bio .
+#docker build -f rdsc_gpu/Dockerfile -t rdsc_gpu .
+#docker build -f rdsc_bio/Dockerfile -t rdsc_bio .
