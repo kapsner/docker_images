b236f286b04b369d76e4d5b5248d89a329823a24
kapsner <lorenz.kapsner@web.de>
2020-01-12 17:39:53 +0100

    modified df architecture

--- Rdatascience/build_rdatascience.sh
+++ Rdatascience/build_rdatascience.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 printf "\nBuild r_datascience\n"
-docker build -f Dockerfile -t rdc_base .
-#docker build -f Dockerfile.gpu -t rdc_gpu .
-#docker build -f Dockerfile.bio -t rdc_bio .
+docker build -f Dockerfile -t rdsc_base .
+#docker build -f rdc_gpu/Dockerfile -t rdsc_gpu .
+#docker build -f rdc_bio/Dockerfile -t rdsc_bio .
