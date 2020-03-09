f0b8d975d6a710b1df481d7ba11d1815125125a7
kapsner <lorenz.kapsner@web.de>
2020-03-09 20:10:16 +0100

    added packages to gpu file

--- Rdatascience/build_rdsc.sh
+++ Rdatascience/build_rdsc.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-printf "\nBuild r_datascience\n"
+printf "\nBuild rdsc_base image\n"
 docker build -f Dockerfile -t rdsc_base .
-#docker build -f bio/Dockerfile -t rdsc_bio .
+
+# printf "\nBuild rdsc_bio image\n"
+# docker build -f bio/Dockerfile -t rdsc_bio .
+
+# printf "\nBuild gpu images\n"
+# docker build -f gpu/Dockerfile-base -t rdsc_gpu_base .
+# docker build -f gpu/Dockerfile -t rdsc_gpu .
--- Rdatascience/gpu/build_rdsc_gpu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-printf "\nBuild gpu images\n"
-docker build -f Dockerfile-base -t rdsc_gpu_base .
-docker build -f Dockerfile -t rdsc_gpu .
