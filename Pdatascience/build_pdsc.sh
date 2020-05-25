838aac36229189f94368f7a23f64c122e847214e
kapsner <lorenz.kapsner@web.de>
2020-05-25 12:44:26 +0200

    updated pdsc

---
+++ Pdatascience/build_pdsc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+printf "\nBuild pdsc_base image\n"
+docker build -f image_pdsc_base/Dockerfile -t pdsc_base .
+
+printf "\nBuild pdsc_headless image\n"
+docker build -f image_pdsc_headless/Dockerfile -t image_pdsc_headless .
---
+++ Pdatascience/build_pdsc_gpu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+printf "\nBuild pdsc_base image\n"
+docker build -f image_pdsc_base/Dockerfile -t pdsc_base .
+
+printf "\nBuild pdsc_gpu_base image\n"
+docker build -f image_pdsc_gpu_base/Dockerfile -t pdsc_gpu_base .
+
+printf "\nBuild pdsc_gpu image\n"
+docker build -f image_pdsc_gpu/Dockerfile -t pdsc_gpu .
