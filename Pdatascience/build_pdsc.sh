76aa126b2791ed09cfe2573e4e62a77003fe9543
kapsner <lorenz.kapsner@web.de>
2020-05-29 08:44:28 +0200

    updated some necessary libraries

--- Pdatascience/build_pdsc.sh
+++ Pdatascience/build_pdsc.sh
@@ -4,4 +4,4 @@ printf "\nBuild pdsc_base image\n"
 docker build -f image_pdsc_base/Dockerfile -t pdsc_base .
 
 printf "\nBuild pdsc_headless image\n"
-docker build -f image_pdsc_headless/Dockerfile -t image_pdsc_headless .
+docker build -f image_pdsc_headless/Dockerfile -t pdsc_headless .
