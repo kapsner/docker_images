dae22d4cca102be72184ce144b2f62d21f0ba198
kapsner <lorenz.kapsner@web.de>
2020-10-23 10:28:23 +0200

    updated gpu install script

--- Pdatascience/build_pdsc_gpu.sh
+++ Pdatascience/build_pdsc_gpu.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-printf "\nBuild pdsc_base image\n"
-docker build -f image_pdsc_base/Dockerfile -t pdsc_base .
-
 printf "\nBuild pdsc_gpu image\n"
 docker build -f image_pdsc_gpu/Dockerfile -t pdsc_gpu .
