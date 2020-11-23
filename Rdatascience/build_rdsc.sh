064f2a9cb431c6ade467d616a4b748d54e5062f6
Jonathan Mang <jonathan.mang@uk-erlangen.de>
2020-11-23 10:57:09 +0100

    refactor: adapted rstudio sttings

--- Rdatascience/build_rdsc.sh
+++ Rdatascience/build_rdsc.sh
@@ -10,46 +10,62 @@ printf "joundso:"
 
 ## Base image:
 IMAGE_NAME=rdsc_base_j
-printf "\nBuilding $IMAGE_NAME image\n"
+printf "\n\n##################################\n"
+printf "$IMAGE_NAME"
+printf "\n##################################\n"
+printf "\nPulling cached $IMAGE_NAME image\n"
 # pull latest image for caching:
 docker pull $REGISTRY_PREFIX/$IMAGE_NAME
 # build new image (latest):
+printf "\n\nBuilding $IMAGE_NAME image\n"
 docker build -f image_$IMAGE_NAME/Dockerfile -t $REGISTRY_PREFIX/$IMAGE_NAME .
-printf "\nPushing $IMAGE_NAME image\n"
+printf "\n\nPushing $IMAGE_NAME image (latest)\n"
 # push new image as new 'latest':
 docker push "$REGISTRY_PREFIX/$IMAGE_NAME"
 # also tag it with the new tag:
 docker tag $REGISTRY_PREFIX/$IMAGE_NAME $REGISTRY_PREFIX/$IMAGE_NAME:$VERSION_TAG
 # and also push this (tagged) image:
+printf "\n\nPushing $IMAGE_NAME image ($VERSION_TAG)\n"
 docker push "$REGISTRY_PREFIX/$IMAGE_NAME:$VERSION_TAG"
 
 
 ## Headless image:
 IMAGE_NAME=rdsc_headless_j
-printf "\nBuilding $IMAGE_NAME image\n"
+printf "\n\n##################################\n"
+printf "$IMAGE_NAME"
+printf "\n##################################\n"
+printf "\nPulling cached $IMAGE_NAME image\n"
 # pull latest image for caching:
 docker pull $REGISTRY_PREFIX/$IMAGE_NAME
 # build new image (latest):
+printf "\n\nBuilding $IMAGE_NAME image\n"
 docker build -f image_$IMAGE_NAME/Dockerfile -t $REGISTRY_PREFIX/$IMAGE_NAME .
-printf "\nPushing $IMAGE_NAME image\n"
+printf "\n\nPushing $IMAGE_NAME image (latest)\n"
 # push new image as new 'latest':
 docker push "$REGISTRY_PREFIX/$IMAGE_NAME"
 # also tag it with the new tag:
 docker tag $REGISTRY_PREFIX/$IMAGE_NAME $REGISTRY_PREFIX/$IMAGE_NAME:$VERSION_TAG
 # and also push this (tagged) image:
+printf "\n\nPushing $IMAGE_NAME image ($VERSION_TAG)\n"
 docker push "$REGISTRY_PREFIX/$IMAGE_NAME:$VERSION_TAG"
+
 
 ## Rstudio image:
 IMAGE_NAME=rdsc_rstudio_j
-printf "\nBuilding $IMAGE_NAME image\n"
+printf "\n\n##################################\n"
+printf "$IMAGE_NAME"
+printf "\n##################################\n"
+printf "\nPulling cached $IMAGE_NAME image\n"
 # pull latest image for caching:
 docker pull $REGISTRY_PREFIX/$IMAGE_NAME
 # build new image (latest):
+printf "\n\nBuilding $IMAGE_NAME image\n"
 docker build -f image_$IMAGE_NAME/Dockerfile -t $REGISTRY_PREFIX/$IMAGE_NAME .
-printf "\nPushing $IMAGE_NAME image\n"
+printf "\n\nPushing $IMAGE_NAME image (latest)\n"
 # push new image as new 'latest':
 docker push "$REGISTRY_PREFIX/$IMAGE_NAME"
 # also tag it with the new tag:
 docker tag $REGISTRY_PREFIX/$IMAGE_NAME $REGISTRY_PREFIX/$IMAGE_NAME:$VERSION_TAG
 # and also push this (tagged) image:
+printf "\n\nPushing $IMAGE_NAME image ($VERSION_TAG)\n"
 docker push "$REGISTRY_PREFIX/$IMAGE_NAME:$VERSION_TAG"
