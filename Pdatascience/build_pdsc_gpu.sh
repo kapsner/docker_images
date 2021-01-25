502b6923368467137035ff343c245eb2c3a7c1c6
kapsner <lorenz.kapsner@web.de>
2021-01-25 11:42:42 +0100

    added jupyter support

--- Pdatascience/build_pdsc_gpu.sh
+++ Pdatascience/build_pdsc_gpu.sh
@@ -6,3 +6,6 @@ cd ../Pdatascience/
 
 printf "\nBuild pdsc_gpu image\n"
 docker build -f image_pdsc_gpu/Dockerfile -t pdsc_gpu .
+
+printf "\nBuild pdsc_jpylab_gpu image\n"
+docker build --build-arg BASEIMAGE=pdsc_gpu:latest -f image_pdsc_jpylab/Dockerfile -t pdsc_jpylab_gpu .
