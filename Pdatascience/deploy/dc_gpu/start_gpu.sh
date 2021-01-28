b35f0864f5bcfbeb4ae1734c228e3635bf525da6
kapsner <lorenz.kapsner@web.de>
2021-01-28 12:18:04 +0100

    updated start script; added stop script

--- Pdatascience/deploy/dc_gpu/start_gpu.sh
+++ Pdatascience/deploy/dc_gpu/start_gpu.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -f ./Dockerfile -t pdsc_jpylab_gpu_run .
+REBUILD_FLAG=$1
+
+if [$REBUILD_FLAG -eq 1]
+then
+    printf "\n\nRebuilding image\n\n"
+    docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -f ./Dockerfile -t pdsc_jpylab_gpu_run .
+else
+    printf "\n\nUsing old image\n\n"
+fi
 
 docker-compose -f docker-compose_GPU.yml up -d
 
---
+++ Pdatascience/deploy/dc_gpu/stop_gpu.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker-compose -f docker-compose_GPU.yml stop
