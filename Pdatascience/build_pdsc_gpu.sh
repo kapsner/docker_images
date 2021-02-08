3fd00e7565710c9766adb0185e83deb8972dd75d
kapsner <lorenz.kapsner@web.de>
2021-02-08 12:57:51 +0100

    updated conda env management

--- Pdatascience/build_pdsc_gpu.sh
+++ Pdatascience/build_pdsc_gpu.sh
@@ -5,10 +5,12 @@ cd ../base_image_gpu/
 cd ../Pdatascience/
 
 printf "\nBuild pdsc_gpu_base image\n"
-docker build --build-arg BASEIMAGE=gpu_base:latest -f image_pdsc_base/Dockerfile -t pdsc_gpu_base .
+# docker build --build-arg BASEIMAGE=gpu_base:latest -f image_pdsc_base/Dockerfile -t pdsc_gpu_base .
+docker build --network host --build-arg BASEIMAGE=gpu_base:latest -f image_pdsc_base/Dockerfile -t pdsc_gpu_base .
 
 printf "\nBuild pdsc_gpu image\n"
-docker build -f image_pdsc_gpu/Dockerfile -t pdsc_gpu .
+#docker build -f image_pdsc_gpu/Dockerfile -t pdsc_gpu .
+docker build --network host -f image_pdsc_gpu/Dockerfile -t pdsc_gpu .
 
 printf "\nBuild pdsc_jpylab_gpu image\n"
 docker build --build-arg BASEIMAGE=pdsc_gpu:latest -f image_pdsc_jpylab/Dockerfile -t pdsc_jpylab_gpu .
