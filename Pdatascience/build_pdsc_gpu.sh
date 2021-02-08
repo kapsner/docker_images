dd8c02a6edf7c21f5ff5e5d133f77b3aab786039
kapsner <lorenz.kapsner@web.de>
2021-02-08 22:57:18 +0100

    Update build_pdsc_gpu.sh

--- Pdatascience/build_pdsc_gpu.sh
+++ Pdatascience/build_pdsc_gpu.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-cd ../base_image_gpu/
-./build_gpu_base.sh
-cd ../Pdatascience/
+printf "\nBuild base_pdsc_gpu_build image\n"
+cd ../base_image_light
+#docker build --build-arg BASEIMAGE=nvidia/cuda:11.0.3-cudnn8-devel-ubuntu20.04 -f Dockerfile -t base_pdsc_gpu_build .
+docker build --network host --build-arg BASEIMAGE=nvidia/cuda:11.0.3-cudnn8-devel-ubuntu20.04 -f Dockerfile -t base_pdsc_gpu_build .
 
+printf "\nBuild pdsc_gpu_build image\n"
+cd ../base_image_gpu/
+docker build --network host --build-arg BASEIMAGE=base_pdsc_gpu_build -f Dockerfile -t pdsc_gpu_build .
+
+# build base image based on nvidia/cuda runtime
+cd ../base_image
+./build_gpu_base.sh
+
+cd ../Pdatascience/
 printf "\nBuild pdsc_gpu_base image\n"
 # docker build --build-arg BASEIMAGE=gpu_base:latest -f image_pdsc_base/Dockerfile -t pdsc_gpu_base .
 docker build --network host --build-arg BASEIMAGE=gpu_base:latest -f image_pdsc_base/Dockerfile -t pdsc_gpu_base .
