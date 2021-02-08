46048204d6b67f92c71a6a1d2706f329eeb07c72
kapsner <lorenz.kapsner@web.de>
2021-02-08 22:17:28 +0100

    Delete build_base_image.sh

--- base_image/build_base_image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-printf "\nBuild base_image image\n"
-#docker build --build-arg BASEIMAGE=ubuntu:20.10 -f Dockerfile -t base_image .
-docker build --network host --build-arg BASEIMAGE=ubuntu:20.10 -f Dockerfile -t base_image .
