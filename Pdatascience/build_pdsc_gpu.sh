d826508150cee00e81a0d02a4ff90cf5b5915b15
kapsner <lorenz.kapsner@web.de>
2021-02-09 16:18:48 +0100

    removed network=host flags from sh scripts

--- Pdatascience/build_pdsc_gpu.sh
+++ Pdatascience/build_pdsc_gpu.sh
@@ -6,24 +6,22 @@ cd ../build_image_gpu
 ./build_base_gpu_build.sh
 
 printf "\nBuild pdsc_gpu_build image\n"
-docker build --network host -f Dockerfile.py -t pdsc_gpu_build .
+docker build -f Dockerfile.py -t pdsc_gpu_build .
 
 # build final image ontop of nvidia/cuda-runtime container
+# (could happen in parallel to first 2 steps in the future)
 cd ../base_image
 ./build_base_image_gpu.sh
 
 cd ../Pdatascience/
 printf "\nBuild pdsc_gpu_multistage image\n"
-# docker build --build-arg BASEIMAGE=gpu_base:latest -f image_pdsc_base/Dockerfile -t pdsc_gpu_base .
-docker build --network host -f image_pdsc_gpu/Dockerfile.multi -t pdsc_gpu_multistage .
+docker build -f image_pdsc_gpu/Dockerfile.multi -t pdsc_gpu_multistage .
 
 printf "\nBuild pdsc_gpu_base image\n"
-# docker build --build-arg BASEIMAGE=gpu_base:latest -f image_pdsc_base/Dockerfile -t pdsc_gpu_base .
-docker build --network host --build-arg BASEIMAGE=pdsc_gpu_multistage:latest -f image_pdsc_base/Dockerfile -t pdsc_gpu_base .
+docker build --build-arg BASEIMAGE=pdsc_gpu_multistage:latest -f image_pdsc_base/Dockerfile -t pdsc_gpu_base .
 
 printf "\nBuild pdsc_gpu image\n"
-#docker build -f image_pdsc_gpu/Dockerfile -t pdsc_gpu .
-docker build --network host -f image_pdsc_gpu/Dockerfile -t pdsc_gpu .
+docker build -f image_pdsc_gpu/Dockerfile -t pdsc_gpu .
 
 printf "\nBuild pdsc_jpylab_gpu image\n"
-docker build --network host --build-arg BASEIMAGE=pdsc_gpu:latest -f image_pdsc_jpylab/Dockerfile -t pdsc_jpylab_gpu .
+docker build --build-arg BASEIMAGE=pdsc_gpu:latest -f image_pdsc_jpylab/Dockerfile -t pdsc_jpylab_gpu .
--- Rdatascience/build_rdsc.sh
+++ Rdatascience/build_rdsc.sh
@@ -5,12 +5,10 @@ cd ../base_image
 cd ../Rdatascience
 
 printf "\nBuild rdsc_base image\n"
-#docker build --build-arg BASEIMAGE=base_image:latest -f image_rdsc_base/Dockerfile -t rdsc_base .
-docker build --network host --build-arg BASEIMAGE=base_image:latest -f image_rdsc_base/Dockerfile -t rdsc_base .
+docker build --build-arg BASEIMAGE=base_image:latest -f image_rdsc_base/Dockerfile -t rdsc_base .
 
 printf "\nBuild rdsc_headless image\n"
-#docker build -f image_rdsc_headless/Dockerfile -t rdsc_headless .
-docker build --network host -f image_rdsc_headless/Dockerfile -t rdsc_headless .
+docker build -f image_rdsc_headless/Dockerfile -t rdsc_headless .
 
 printf "\nBuild rdsc_rstudio image\n"
 docker build --build-arg BASEIMAGE=rdsc_headless:latest -f image_rdsc_rstudio/Dockerfile -t rdsc_rstudio .
