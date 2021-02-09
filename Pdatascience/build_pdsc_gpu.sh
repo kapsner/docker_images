62ae5a4fa8c7481741a8b1dd33ef6ba30e6884bf
kapsner <lorenz.kapsner@web.de>
2021-02-09 12:52:09 +0100

    temporarily added network host to last pdsc-gpu cmd

--- Pdatascience/build_pdsc_gpu.sh
+++ Pdatascience/build_pdsc_gpu.sh
@@ -26,4 +26,4 @@ printf "\nBuild pdsc_gpu image\n"
 docker build --network host -f image_pdsc_gpu/Dockerfile -t pdsc_gpu .
 
 printf "\nBuild pdsc_jpylab_gpu image\n"
-docker build --build-arg BASEIMAGE=pdsc_gpu:latest -f image_pdsc_jpylab/Dockerfile -t pdsc_jpylab_gpu .
+docker build --network host --build-arg BASEIMAGE=pdsc_gpu:latest -f image_pdsc_jpylab/Dockerfile -t pdsc_jpylab_gpu .
