1b8baec16c73b6ea4a315f86c91cca573ba5607a
kapsner <lorenz.kapsner@web.de>
2021-05-19 16:28:43 +0200

    updated base df

--- base_image/build_base_image.sh
+++ base_image/build_base_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 printf "\nBuild base_image image\n"
-docker build --build-arg BASEIMAGE=ubuntu:20.10 -f Dockerfile -t base_image .
+docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) --build-arg BASEIMAGE=ubuntu:20.10 -f Dockerfile -t base_image .
