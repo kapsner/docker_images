16aa1dbadd1209082ec2daa61f9f5bf7b491b87d
kapsner <lorenz.kapsner@web.de>
2021-11-04 08:52:22 +0100

    removed commented code

--- base_image/build_base_image_gpu.sh
+++ base_image/build_base_image_gpu.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 printf "\nBuild base_image_gpu image\n"
-# docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) --build-arg BASEIMAGE=nvidia/cuda:11.0.3-cudnn8-runtime-ubuntu20.04 -f Dockerfile -t base_image_gpu .
 docker build --build-arg BASEIMAGE=nvidia/cuda:11.0.3-cudnn8-runtime-ubuntu20.04 -f Dockerfile -t base_image_gpu .
