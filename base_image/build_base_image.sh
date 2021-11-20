65b18be033858073364dc6c07ba293c9fa30a329
kapsner <lorenz.kapsner@web.de>
2021-11-20 10:02:11 +0100

    moving to ubuntu 21.04

--- base_image/build_base_image.sh
+++ base_image/build_base_image.sh
@@ -2,4 +2,4 @@
 
 printf "\nBuild base_image image\n"
 # docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) --build-arg BASEIMAGE=ubuntu:20.10 -f Dockerfile -t base_image .
-docker build --build-arg BASEIMAGE=ubuntu:20.10 -f Dockerfile -t base_image .
+docker build --build-arg BASEIMAGE=ubuntu:21.04 -f Dockerfile -t base_image .
