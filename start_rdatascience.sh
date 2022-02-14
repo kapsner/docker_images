88e659dfdfcbe039f38a1f4dcbf2188ec2decdd0
Jonathan Mang <56686638+joundso@users.noreply.github.com>
2022-02-14 12:34:36 +0100

    chore: added docker pull to ensure newest local image before starting

--- start_rdatascience.sh
+++ start_rdatascience.sh
@@ -6,10 +6,17 @@
 
 source ./.env
 
+file="docker-compose.yml"
+
 if [ -f "$CUSTOM_DOCKER_COMPOSE_FILE" ]; then
-    echo "Custom docker-compose file $CUSTOM_DOCKER_COMPOSE_FILE exists. Starting it now."
-    docker-compose -f $CUSTOM_DOCKER_COMPOSE_FILE up -d
+    echo "Custom docker-compose file ${CUSTOM_DOCKER_COMPOSE_FILE} exists. Starting it now."
+    file=${CUSTOM_DOCKER_COMPOSE_FILE}
 else 
-    echo "NO custom docker-compose file exists. Starting the default now."
-    docker-compose up -d
+    echo "NO custom docker-compose file exists. Starting the default '${file}' now."
 fi
+
+## Download the newest image(s):
+docker-compose -f ${file} pull
+
+## Spin them up:
+docker-compose -f ${file} up -d
