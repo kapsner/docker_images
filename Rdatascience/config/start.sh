e82611b6a27abdfe0e657543c383758cdcc2f2dd
kapsner <lorenz.kapsner@web.de>
2022-07-12 19:32:17 +0200

    removed ssl for url to tunnel rstudio

--- Rdatascience/config/start.sh
+++ Rdatascience/config/start.sh
@@ -3,6 +3,6 @@
 printf "\nStarting RStudio server\n\n"
 rstudio-server start
 
-printf "\e]8;;https://0.0.0.0:8787\e\\https://0.0.0.0:8787\e]8;;\e\\n"
+printf "\e]8;;http://0.0.0.0:8787\e\\http://0.0.0.0:8787\e]8;;\e\\n"
 
 tail -f /dev/null 
