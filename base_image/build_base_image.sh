97388343c50dad1ce4081bbb9703ad220da3f3e9
kapsner <lorenz.kapsner@web.de>
2024-10-15 21:53:27 +0200

    feat: updated to ubuntu24.04

--- base_image/build_base_image.sh
+++ base_image/build_base_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 printf "\nBuild base_image image\n"
-docker build --build-arg BASEIMAGE=ubuntu:22.04 -f Dockerfile -t base_image .
+docker build --build-arg BASEIMAGE=ubuntu:24.04 -f Dockerfile -t base_image .
